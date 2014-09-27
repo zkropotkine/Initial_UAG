665984ba1d78ef67e9efbce0f66be4d14907ccab
zkropotkine <s.daniel.rodriguez@gmail.com>
2014-09-27 12:53:22 -0500

    Initial Commit

---
+++ UAG/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  UAG
+//
+//  Created by Daniel on 9/27/14.
+//  Copyright (c) 2014 CordiSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ UAG/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  UAG
+//
+//  Created by Daniel on 9/27/14.
+//  Copyright (c) 2014 CordiSoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
